d5d92f88e4e9cc88b48ed1ce1900700885d53f60
Kristoffer Løyning Bøe <kristoffer.boe@bouvet.no>
2020-12-17 08:11:16 +0100

    Adding day 4 and 5

---
+++ Day4/Day4Solution/Day4Solution/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day4Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "C:/Users/kristoffer.boe/krisbo_git/AdventOfCode2020/Day4/Day4Data.txt";
+            string passports_string = System.IO.File.ReadAllText(filepath);
+
+            string[] passports = passports_string.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            int number_of_valid_passports_part_1 = CountValidPassportsPart1(passports);
+            int number_of_valid_passports_part_2 = CountValidPassportsPart2(passports);
+
+            Console.WriteLine("Number of valid passports part 1: "+ number_of_valid_passports_part_1.ToString());
+            Console.WriteLine("Number of valid passports part 2: " + number_of_valid_passports_part_2.ToString());
+        }
+
+        private static int CountValidPassportsPart2(string[] passports)
+        {
+            int number_of_valid_passports = 0;
+
+            foreach (string passport in passports)
+            {
+                string passport_lower = passport.ToLower();
+                if (passport_lower.Contains("byr") &&
+                    passport_lower.Contains("iyr") &&
+                    passport_lower.Contains("eyr") &&
+                    passport_lower.Contains("hgt") &&
+                    passport_lower.Contains("hcl") &&
+                    passport_lower.Contains("ecl") &&
+                    passport_lower.Contains("pid"))
+                {
+                    string[] passport_splitted = passport_lower.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    bool password_is_valid = true;
+                    foreach(string entry in passport_splitted)
+                    {
+                        string[] entry_splitted = entry.Trim().Split(':');
+                        if(entry_splitted[0] == "byr" && !(Convert.ToInt32(entry_splitted[1]) >= 1920 && Convert.ToInt32(entry_splitted[1]) <= 2002)) {
+                            password_is_valid = false;
+                            break;
+                        }
+                        if (entry_splitted[0] == "iyr" && !(Convert.ToInt32(entry_splitted[1]) >= 2010 && Convert.ToInt32(entry_splitted[1]) <= 2020))
+                        {
+                            password_is_valid = false;
+                            break;
+                        }
+                        if (entry_splitted[0] == "eyr" && !(Convert.ToInt32(entry_splitted[1]) >= 2020 && Convert.ToInt32(entry_splitted[1]) <= 2030))
+                        {
+                            password_is_valid = false;
+                            break;
+                        }
+                        if (entry_splitted[0] == "hgt" && 
+                            !((entry_splitted[1].Contains("cm") 
+                                && Convert.ToInt32(entry_splitted[1].Replace("cm", "")) >= 150 && Convert.ToInt32(entry_splitted[1].Replace("cm", "")) <= 193) || 
+                             (entry_splitted[1].Contains("in")
+                                && Convert.ToInt32(entry_splitted[1].Replace("in", "")) >= 59 && Convert.ToInt32(entry_splitted[1].Replace("in", "")) <= 76)))
+                        {
+                            password_is_valid = false;
+                            break;
+                        }
+                        if (entry_splitted[0] == "hcl" && !(new Regex("^#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]$").IsMatch(entry_splitted[1])))
+                        {
+                            password_is_valid = false;
+                            break;
+                        }
+                        if (entry_splitted[0] == "ecl" && !(Array.Exists(new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }, element => element == entry_splitted[1])))
+                        {
+                            password_is_valid = false;
+                            break;
+                        }
+                        if (entry_splitted[0] == "pid" && !(new Regex("^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$").IsMatch(entry_splitted[1])))
+                        {
+                            password_is_valid = false;
+                            break;
+                        }
+                    }
+
+                    if (password_is_valid)
+                    {
+                        number_of_valid_passports += 1;
+                    }
+                }
+            }
+
+            return number_of_valid_passports;
+        }
+
+        private static int CountValidPassportsPart1(string[] passports)
+        {
+            int number_of_valid_passports = 0;
+
+            foreach(string passport in passports)
+            {
+                string passport_lower = passport.ToLower();
+                if (passport_lower.Contains("byr") &&
+                    passport_lower.Contains("iyr") &&
+                    passport_lower.Contains("eyr") &&
+                    passport_lower.Contains("hgt") &&
+                    passport_lower.Contains("hcl") &&
+                    passport_lower.Contains("ecl") &&
+                    passport_lower.Contains("pid"))
+                {
+                    number_of_valid_passports += 1;
+                }
+            }
+
+            return number_of_valid_passports;
+        } 
+    }
+}
---
+++ Day5/Day5Solution/Day5Solution/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day5Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(GetSeatRow("FBFBBFF"));
+            //Console.WriteLine(GetSeatColumn("RLR"));
+
+            //Console.WriteLine(GetSeatNumber("BFFFBBFRRR"));
+            //Console.WriteLine(GetSeatNumber("FFFBBBFRRR"));
+            //Console.WriteLine(GetSeatNumber("BBFFBBFRLL"));
+
+            string filepath = "C:/Users/kristoffer.boe/krisbo_git/AdventOfCode2020/Day5/Day5Data.txt";
+            string[] flight_seats = System.IO.File.ReadAllLines(filepath);
+
+            int highest_seat_id = 0;
+            List<int> seats = new List<int>();
+
+            foreach(string flight_seat in flight_seats)
+            {
+                int current_seat_id = GetSeatNumber(flight_seat);
+                seats.Add(current_seat_id);
+                if (current_seat_id > highest_seat_id)
+                {
+                    highest_seat_id = current_seat_id;
+                }
+            }
+            
+            Console.WriteLine("Highest seat ID in dataset: " + highest_seat_id.ToString());
+            seats.Sort();
+
+            int[] steats_array = seats.ToArray();
+
+            Console.WriteLine("My seat ID is: " + GetMissingSeat(steats_array).ToString());
+        }
+
+        private static int GetMissingSeat(int[] steats_array)
+        {
+            for (int i = 0; i < steats_array.Length - 1; i++)
+            {
+                if (steats_array[i] + 2 == steats_array[i + 1])
+                {
+                    return steats_array[i] + 1;
+                }
+            }
+            return -1;
+        }
+
+        private static int GetSeatNumber(string binary_space_partitioning)
+        {
+            string row_binary_space_partitioning = binary_space_partitioning.Substring(0, 7);
+            string column_binary_space_partitioning = binary_space_partitioning.Substring(7, 3);
+
+            int row = GetSeatRow(row_binary_space_partitioning);
+            int column = GetSeatColumn(column_binary_space_partitioning);
+
+            int seat_id = row * 8 + column;
+
+            //Console.WriteLine(binary_space_partitioning + ": row " + row.ToString() + ", column " + column.ToString() + ", seat ID " + seat_id);
+
+            return seat_id;
+        }
+
+        private static int GetSeatRow(string row, int row_range_min = 0, int row_range_max = 127)
+        {
+            //Console.WriteLine("row: " + row + ", row_range_min: " + row_range_min.ToString() + ", row_range_max: " + row_range_max.ToString());
+            if (row.Length == 0)
+            {
+                return row_range_min;
+            }
+            if (row[0] == 'F')
+            {
+                return GetSeatRow(row.Remove(0, 1), row_range_min, Convert.ToInt32(Math.Floor(Convert.ToDouble(row_range_min) + ((Convert.ToDouble(row_range_max) - Convert.ToDouble(row_range_min)) / 2))));
+            }
+            if (row[0] == 'B')
+            {
+                return GetSeatRow(row.Remove(0, 1), Convert.ToInt32(Math.Ceiling(Convert.ToDouble(row_range_min) + ((Convert.ToDouble(row_range_max) - Convert.ToDouble(row_range_min)) / 2))), row_range_max);
+            }
+            return row_range_min;
+        }
+
+        private static int GetSeatColumn(string column, int column_range_min = 0, int column_range_max = 7)
+        {
+            //Console.WriteLine("column: " + column + ", column_range_min: " + column_range_min.ToString() + ", column_range_max: " + column_range_max.ToString());
+            if (column.Length == 0)
+            {
+                return column_range_min;
+            }
+            
+            if (column[0] == 'L')
+            {
+                return GetSeatColumn(column.Remove(0, 1), column_range_min, Convert.ToInt32(Math.Floor(Convert.ToDouble(column_range_min) + ((Convert.ToDouble(column_range_max) - Convert.ToDouble(column_range_min)) / 2))));
+            }
+            if (column[0] == 'R')
+            {
+                return GetSeatColumn(column.Remove(0, 1), Convert.ToInt32(Math.Ceiling(Convert.ToDouble(column_range_min) + ((Convert.ToDouble(column_range_max) - Convert.ToDouble(column_range_min)) / 2))), column_range_max);
+            }
+            return column_range_min;
+        }
+    }
+}
